6a2ad1eac755ebe4b9d297a9ddb2b9876ecfc533
ryanlaw <ryan_law-rebound@hotmail.com>
2023-01-31 13:48:47 +0800

    removed settings.py, added ebay api call services

---
+++ main/services/ebay_api_services.py
@@ -0,0 +1,30 @@
+import requests
+
+keyword = "charizard psa 10"
+maxEntries = 10
+
+def get_data_from_api(keyword, maxEntries):
+    url = (
+        "http://svcs.ebay.com/services/search/FindingService/v1?"
+        f"OPERATION-NAME=findItemsByKeywords&"
+        f"SERVICE-VERSION=1.0.0&"
+        f"SECURITY-APPNAME={appId}&"
+        "RESPONSE-DATA-FORMAT=JSON&"
+        "REST-PAYLOAD&"
+        f"paginationInput.entriesPerPage={maxEntries}&"
+        f"keywords={keyword}"
+    )
+
+    response = requests.get(url)
+    try:
+        if response.status_code == 200:
+            data = response.json()
+            if "findItemsByKeywordsResponse" in data:
+                listings = data["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]
+                return listings
+            else:
+                raise Exception("Error: Invalid response data")
+        else:
+            raise Exception("Error: Invalid response status code")
+    except Exception as e:
+        return e
