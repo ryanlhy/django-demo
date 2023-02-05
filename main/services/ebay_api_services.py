0da9b49b763209d4e9fc8a296439f8edba65de3c
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-05 22:45:24 +0800

    added handl negative keyword function

--- main/services/ebay_api_services.py
+++ main/services/ebay_api_services.py
@@ -1,10 +1,11 @@
 import requests
 from main.settings import ebay_app_id as appId
+import json
 
 keyword = "charizard psa 10"
 maxEntries = 10
 #https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME={appid}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=100&keywords=pikachu%22
-def get_data_from_ebay_api(keyword, maxEntries=10):
+def get_data_from_ebay_api(keyword, maxEntries=100):
     url = (
         "http://svcs.ebay.com/services/search/FindingService/v1?"
         f"OPERATION-NAME=findItemsByKeywords&"
@@ -20,8 +21,10 @@ def get_data_from_ebay_api(keyword, maxEntries=10):
     try:
         if response.status_code == 200:
             data = response.json()
+            # data = json.loads(response.text) #deserialise the data
             if "findItemsByKeywordsResponse" in data:
                 listings = data["findItemsByKeywordsResponse"][0]["searchResult"][0]["item"]
+                listings = handle_negative_keywords("cgc", listings)
                 return listings
                 # return data
             else:
@@ -29,4 +32,16 @@ def get_data_from_ebay_api(keyword, maxEntries=10):
         else:
             raise Exception("Error: Invalid response status code")
     except Exception as e:
+
         return e
+
+def handle_negative_keywords(negativeKeyword, originalData):
+    neg_indexes = []
+    for index, item in enumerate(originalData):
+        title = item["title"][0].lower() # need to do error handling here
+        if negativeKeyword in title.lower():
+            neg_indexes.append(index)
+    # append the arr to the originalData
+    originalData.append({"negativeKeyword": neg_indexes})
+    print(originalData)
+    return originalData
