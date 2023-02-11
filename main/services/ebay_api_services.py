baf15743241492fb4b93f03dd7e011de234560e7
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-11 11:12:57 +0800

    commented out negative keywords function and used the function in utils

--- main/services/ebay_api_services.py
+++ main/services/ebay_api_services.py
@@ -35,12 +35,12 @@ def get_data_from_ebay_api(keyword, maxEntries=100):
 
         return e
 
-def handle_negative_keywords(negativeKeyword, listingsData):
-    neg_indexes = []
-    negativeKeyword.lower()
-    for index, item in enumerate(listingsData):
-        title = item["title"][0].lower() # need to do error handling here
-        print(title)
-        if negativeKeyword in title.lower():
-            neg_indexes.append(index)
-    return neg_indexes
+# def handle_negative_keywords(negativeKeyword, listingsData):
+#     neg_indexes = []
+#     negativeKeyword.lower()
+#     for index, item in enumerate(listingsData):
+#         title = item["title"][0].lower() # need to do error handling here
+#         print(title)
+#         if negativeKeyword in title.lower():
+#             neg_indexes.append(index)
+#     return neg_indexes
