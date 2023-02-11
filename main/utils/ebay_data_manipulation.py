3b67d715849d16c9f5c33dedbedb206481db6c9f
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-11 11:11:31 +0800

    uncommented function to import

--- main/utils/ebay_data_manipulation.py
+++ main/utils/ebay_data_manipulation.py
@@ -1,10 +1,11 @@
-# def handle_negative_keywords(negativeKeyword, originalData):
-#     neg_indexes = []
-#     for index, item in enumerate(originalData):
-#         title = item["title"][0].lower() # need to do error handling here
-#         if negativeKeyword in title.lower():
-#             neg_indexes.append(index)
-#     # append the arr to the originalData
-#     originalData.append({"negativeKeyword": neg_indexes})
-#     print(originalData)
-#     return originalData
+def handle_negative_keywords(negativeKeyword, listingsData):
+    neg_indexes = []
+    negativeKeyword.lower()
+    for index, item in enumerate(listingsData):
+        title = item["title"][0].lower() # need to do error handling here
+        print(title)
+        if negativeKeyword in title.lower():
+            neg_indexes.append(index)
+    return neg_indexes
+
+# handle search query - tokenisaiont? break down the query into keywords?
