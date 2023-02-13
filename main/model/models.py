24ede17c282bf651f3f7d54c779ed0ae03737b58
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-13 17:15:20 +0800

    added cardsets model

--- main/model/models.py
+++ main/model/models.py
@@ -5,3 +5,17 @@ class Employee(models.Model):
     name = models.CharField(max_length=100)
     job_title = models.CharField(max_length=50)
     income = models.BigIntegerField()
+    testing = models.CharField(max_length=50, default='testing')
+
+class CardSets(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=50, null=True)
+    series = models.CharField(max_length=50, null=True)
+    printed_total = models.IntegerField(null=True)
+    total = models.IntegerField(null=True)
+    unlimited_legality = models.CharField(max_length=50, null=True)
+    ptcgo_code = models.CharField(max_length=20, null=True)
+    release_date = models.DateField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    symbol_image_url = models.CharField(max_length=200, null=True)
+    logo_image_url = models.CharField(max_length=200, null=True)
