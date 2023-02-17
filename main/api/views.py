bd6bcf5301191d0471603a03496fe1ff47a9b8f5
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-17 18:07:07 +0800

    added test param

--- main/api/views.py
+++ main/api/views.py
@@ -6,10 +6,10 @@ from django.core.serializers import serialize
 from .helpers import GetBody
 from django.forms.models import model_to_dict
 import json
-from services.services_tcg import get_data_from_api
+from services.poke_api_services import get_data_from_api
 from services.ebay_api_services import get_data_from_ebay_api
 # from services.ebay_api_services import handle_negative_keywords
-from utils.ebay_data_manipulation import handle_negative_keywords
+from utils.ebay_data_manipulation import handle_negative_keywords, main_filter_keywords, card_sets1
 
 class EmployeeView(View):
     def get(self, request):
@@ -27,14 +27,17 @@ class PokemonView(View):
     def get(self, request, param):
         query = request.GET.get("query", "no query") ## Grab query from url query
         data = get_data_from_api(param)
-        return JsonResponse({"param": param, "query": query, "data": data})
+        return JsonResponse({"param": param, "query": query, "data": data["data"]})
 
 class EbayView(View):
     def get(self, request, param):
         query = request.GET.get("query", "no query") ## Grab query from url query
-        data = get_data_from_ebay_api(param)
+        param = json.loads(param)
+        data = get_data_from_ebay_api(param["search"])
         negative_keywords = handle_negative_keywords("cgc", data)
-        return JsonResponse({"param": param, "query": query, "data": data, "negative_keywords_index": negative_keywords})
+        # make a check if searchObj exist, then run filter
+        # filter = main_filter_keywords(data, param)
+        return JsonResponse({"param": param, "query": query, "data": data, "negative_keywords_index": negative_keywords, })
 
 class CardSetsView(View):
     def get(self, request):
@@ -43,5 +46,16 @@ class CardSetsView(View):
 
 class TestView(View):
     def get(self, request):
-        return JsonResponse({"message": "Hello, world! Test worked"})
-    
+        # param = serialize("json", param)
+        # card_sets1
+        # return JsonResponse({"message": "Hello, world! Test worked","card":card_sets1})
+        # return card_sets1
+        # return JsonResponse(json.loads(serialize("json", card_sets1)), safe=False)
+        return JsonResponse({"message":"param"})
+
+class TestParamView(View):
+    def get(self, request, param):
+        # print(param)
+        param_decode = json.loads(param)
+        # param = serialize("json", param)
+        return JsonResponse({"message":param_decode})
