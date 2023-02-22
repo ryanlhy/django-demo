55f09f01f61b7ff3daab21225f1991202bda1ea1
ryanlaw <ryan_law-rebound@hotmail.com>
2023-02-22 15:28:27 +0800

    swap grades from larges to smallest

--- main/utils/data.py
+++ main/utils/data.py
@@ -1,7 +1,7 @@
 variation = ["holofoil", "reverse holo", "1st edition", "1st ed", "1st"]
 
 grading_company = ["psa", "cgc", "bgs", "beckett"]
-grades = ['1', '1.5', '2', '2.5', '3', '3.5', '4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10']
+grades = ['10', '9.5', '9', '8.5', '8', '7.5', '7', '6.5', '6', '5.5', '5', '4.5', '4', '3.5', '3', '2.5', '2', '1.5', '1']
 
 def combine_grade(grading_company, grade):
     combined_list = []
