c55a1524e3f90fca7a4e62600569631ce4e82e01
ryanlaw <ryan_law-rebound@hotmail.com>
2023-04-08 16:28:11 +0800

    swapped order of OrderDetails and Order class

--- main/model/models.py
+++ main/model/models.py
@@ -51,13 +51,6 @@ class CartDetails(models.Model):
     grade = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
 
-class Orders(models.Model):
-    id = models.AutoField(primary_key=True)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
-    order_details = models.ForeignKey(OrderDetails, on_delete=models.CASCADE, related_name='orders')
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-    date_created = models.DateTimeField(auto_now_add=True)
-
 class OrderDetails(models.Model):
     id = models.AutoField(primary_key=True)
     order = models.ForeignKey(Orders, on_delete=models.CASCADE, related_name='order_details')
@@ -67,3 +60,10 @@ class OrderDetails(models.Model):
     pokemon_id = models.CharField(max_length=15)
     grade = models.CharField(max_length=20)
     date_added = models.DateTimeField(auto_now_add=True)
+
+class Orders(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    order_details = models.ForeignKey(OrderDetails, on_delete=models.CASCADE, related_name='orders')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
